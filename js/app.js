38a18dd92268e2338a3ed3f14975ac9ccaef21dd
Harry Nguyen <phuquyit@gmail.com>
2020-02-26 22:22:03 +0700

    search v1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let name = ["Nguyen Van Tan", "Nguyen Van Ta", "Nguyen Thi Yen "];
+let search1 = [];
 
 function listNv() {
     let html = "";
@@ -42,9 +43,37 @@ function deleteNV(id) {
     name.splice(id, 1);
     listNv();
 }
+function search2() {
+    let html = "";
+    for (let i = 0; i < search1.length; i++) {
+        html += "<tr>";
+        html += "<td>";
+        html += i + 1;
+        html += "</td>";
+        html += "<td>";
+        html += name[i];
+        html += "</td>";
+        html += "<td>";
+        html += "<button id = '" + i + "' onclick = 'repair(this.id)'>";
+        html += "Repair";
+        html += "</buttom>";
+        html += "</td>";
+        html += "<td>";
+        html += "<button id = '" + i + "' onclick = 'deleteNV(this.id)'>";
+        html += "Delete";
+        html += "</buttom>";
+        html += "</td>";
+        html += "</tr>";
+    }
+    document.getElementById("listNv1").innerHTML = html;
+}
 
 function search() {
-    let str = document.getElementById("addname").value;
-    name = str.search;
-        listNv();
+    let str = "";
+    for (let i = 0; i < name.length; i++) {
+     str += name[i];
+    }
+    let str2 = str.search(document.getElementById("addname").value);
+    search1.unshift(str2);
+    search2();
 }
